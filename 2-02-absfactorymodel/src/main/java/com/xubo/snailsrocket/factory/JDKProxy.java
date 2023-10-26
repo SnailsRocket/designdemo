a2a2a7cbd8fca82163ffbfe548b0b6efbe6926b5
xubo <xubo@ecdigit.com>
2023-10-26 16:02:54 +0800

    对 getInterfaces() 认识不足

--- 2-02-absfactorymodel/src/main/java/com/xubo/snailsrocket/factory/JDKProxy.java
+++ 2-02-absfactorymodel/src/main/java/com/xubo/snailsrocket/factory/JDKProxy.java
@@ -14,6 +14,7 @@ public class JDKProxy {
     public static <T> T getProxy(Class<T> interfaceClass, ICacheAdapter cacheAdapter) throws Exception {
         InvocationHandler handler = new JDKInvocationHandler(cacheAdapter);
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        // 获取当前对象实现的第一个接口
         Class<?>[] classes = interfaceClass.getInterfaces();
         return (T) Proxy.newProxyInstance(classLoader, new Class[]{classes[0]}, handler);
     }
--- 2-02-absfactorymodel/src/test/java/com/xubo/snailsrocket/ApiTest.java
+++ 2-02-absfactorymodel/src/test/java/com/xubo/snailsrocket/ApiTest.java
@@ -16,12 +16,13 @@ public class ApiTest {
 
     @Test
     public void test_CacheService() throws Exception {
+        // getInterfaces
         CacheService proxy_EGM = JDKProxy.getProxy(CacheServiceImpl.class, new EGMCacheAdapter());
         proxy_EGM.set("user_name_01", "Druid");
         String val01 = proxy_EGM.get("user_name_01");
         System.out.println("val01 = " + val01);
 
-        // 为什么返回 CacheServiceImpl 就异常
+        // 为什么返回 CacheServiceImpl 就异常   反射 之 getInterfaces() 获取当期对象实现的第一个接口
         CacheServiceImpl proxy_IIR = JDKProxy.getProxy(CacheServiceImpl.class, new IIRCacheAdapter());
         proxy_IIR.set("user_name_02", "snails");
         String val02 = proxy_IIR.get("user_name_02");
