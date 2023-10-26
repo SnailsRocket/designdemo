1c129ad842824eb15173dbbccfcc56f28d21043f
xubo <xubo@ecdigit.com>
2023-10-26 19:37:50 +0800

    注释

--- 3-02-buildermodel/src/test/java/com/xubo/snailsrocket/ApiTest.java
+++ 3-02-buildermodel/src/test/java/com/xubo/snailsrocket/ApiTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 /**
  * @Author xubo
  * @Date 2023/10/26 17:08
- * @Description：使用构造者模式实现
+ * @Description：使用构造者模式实现 实际场景像创建订单就可以使用建造者模式，将一些流程拆分成一个个的工序，然后将他们组装起来。eg：创建订单、计算优惠金额、计算价格、扣减库存、获取收货人信息
  * @Version 1.0
  */
 public class ApiTest {
